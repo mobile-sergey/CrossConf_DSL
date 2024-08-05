a3c6ffa668ad0a97e3f103ff01ec1fc3e5b4a43b
Sergey Lavrov <lavrov-sergey@yandex.ru>
2024-08-05 23:25:31 +0300

    First version of App

---
+++ app/src/main/kotlin/dsl/crossconf/LaunchActivity.kt
@@ -0,0 +1,32 @@
+package dsl.crossconf
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+        val duration = 2000L
+        val handler = Handler(Looper.myLooper()!!)
+
+        val textVersion = findViewById<TextView>(R.id.textVersion)
+        textVersion.text = App.version
+
+        handler.postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },
+            duration
+        )
+    }
+}
---
+++ app/src/main/kotlin/dsl/crossconf/MainActivity.kt
@@ -0,0 +1,22 @@
+package dsl.crossconf
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            textView.text = (1..6).random().toString()
+        }
+    }
+}
